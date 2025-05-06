eb1a12c8b286b6ecd705e63a5e6f7aad97071378
Thibo-Voorhaen <thibo.voorhaen@gmail.com>
2025-05-06 13:35:19 +0200

    Added more ingredients

--- LostInTheSauce/Assets/Scripts/S_T_ItemManager.cs
+++ LostInTheSauce/Assets/Scripts/S_T_ItemManager.cs
@@ -5,7 +5,19 @@ public enum Food
 {
     Tomato = 0,
     Meat = 1,
-    Dough = 2
+    Dough = 2,
+    Cheese = 3,
+    Lettuce = 4,
+    Fish = 5,
+    Rice = 6,
+    Sugar = 7,
+    Cucumber = 8,
+    Potato = 9,
+    Egg = 10,
+    Milk = 11,
+    Oil = 12,
+    Flour = 13,
+    Cocoa = 14
 }
 
 public class S_T_ItemManager : MonoBehaviour
