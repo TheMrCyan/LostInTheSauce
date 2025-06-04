fc0bc20c6eda590e1b85df65ce1e48e67a31568a
Thibo-Voorhaen <thibo.voorhaen@gmail.com>
2025-06-04 21:47:20 +0200

    Added back timers and fixed game over screen

--- LostInTheSauce/Assets/Scripts/S_R_GameOver.cs
+++ LostInTheSauce/Assets/Scripts/S_R_GameOver.cs
@@ -1,6 +1,13 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class DayCounter : MonoBehaviour
+public class S_R_GameOver : MonoBehaviour
 {
-    //load day one scene here
+
+   public void MainMenuButton()
+    {
+        SceneManager.LoadScene("R_HomeScreen");
+        S_R_NextDay.m_DayCount = 2;
+
+    }
 }
